37ac152e3c5cd992c5d025ca985fad5062715281
Christian Persson <chrissp_@hotmail.com>
2018-12-03 23:02:35 +0100

    initial commit

---
+++ Labb2OOAD/Views/CameraPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Labb2OOAD.Views
+{
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Labb2OOAD/Views/SearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Labb2OOAD.Views
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
