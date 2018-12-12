4bc3c9f5e0c12dd130fe33106629caee5a2b0a0f
Christian Persson <chrissp_@hotmail.com>
2018-12-12 23:51:50 +0100

    added imagebutton with animations

--- Labb2OOAD/Views/WelcomePage.xaml.cs
+++ Labb2OOAD/Views/WelcomePage.xaml.cs
@@ -11,5 +11,17 @@ namespace Labb2OOAD.Views
         {
             InitializeComponent();
         }
+
+        async void AnimatePoo(object sender, System.EventArgs e)
+        {
+            //await pooButton.TranslateTo(100, 0, 500, Easing.BounceOut);
+            //await pooButton.TranslateTo(0, 0, 500, Easing.BounceIn);
+            await pooButton.ScaleTo(8, 1000);
+            await pooButton.ScaleTo(5, 1000, Easing.SpringIn);
+            await pooButton.RotateTo(720);
+            await pooButton.RotateTo(0, 1000);
+            await pooButton.FadeTo(0, 1000, Easing.SinOut);
+            await pooButton.FadeTo(100, 1000, Easing.SinIn);
+        }
     }
 }
